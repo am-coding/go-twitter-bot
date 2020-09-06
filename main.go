8d98374eff93870c5ca0e80fa53d2372b35aecd4
am-coding <amrikdeveloper@gmail.com>
2020-09-06 23:42:18 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/dghubble/oauth1"
+)
+
+type Credentials struct {
+	ConsumerKey       string
+	ConsumerSecret    string
+	AccessToken       string
+	AccessTokenSecret string
+}
+
+func getClient(creds *Credentials) (*twitter.client, error) {
+	config := oauth1.NewConfig(creds.ConsumerKey, creds.ConsumerSecret)
+	token := oauth1.NewToken(creds.AccessToken, creds.AccessTokenSecret)
+}
