8efc84cf884dfa486441751a88d4cabe3d9bd175
am-coding <amrikdeveloper@gmail.com>
2020-09-07 00:07:02 +0530

    adding auth

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 )
 
+//access control creds
 type Credentials struct {
 	ConsumerKey       string
 	ConsumerSecret    string
@@ -11,7 +17,48 @@ type Credentials struct {
 	AccessTokenSecret string
 }
 
-func getClient(creds *Credentials) (*twitter.client, error) {
+func getClient(creds *Credentials) (*twitter.Client, error) {
 	config := oauth1.NewConfig(creds.ConsumerKey, creds.ConsumerSecret)
 	token := oauth1.NewToken(creds.AccessToken, creds.AccessTokenSecret)
+
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+
+	// Verify Credentials
+	verifyParams := &twitter.AccountVerifyParams{
+		SkipStatus:   twitter.Bool(true),
+		IncludeEmail: twitter.Bool(true),
+	}
+
+	user, _, err := client.Accounts.VerifyCredentials(verifyParams)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("User's ACCOUNT:\n%+v\n", user)
+	return client, nil
+
+}
+
+func main() {
+	fmt.Println("Go-Twitter Bot v0.01")
+	creds := Credentials{
+		AccessToken:       os.Getenv("ACCESS_TOKEN"),
+		AccessTokenSecret: os.Getenv("ACCESS_TOKEN_SECRET"),
+		ConsumerKey:       os.Getenv("CONSUMER_KEY"),
+		ConsumerSecret:    os.Getenv("CONSUMER_SECRET"),
+	}
+
+	fmt.Printf("%+v\n", creds)
+
+	client, err := getClient(&creds)
+	if err != nil {
+		log.Println("Error getting Twitter Client")
+		log.Println(err)
+	}
+
+	// Print out the pointer to our client
+	// for now so it doesn't throw errors
+	fmt.Printf("%+v\n", client)
+
 }
